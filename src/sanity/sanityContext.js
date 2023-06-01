88173cef397e970d21c7a99e5db6849be047f864
cir52 <47673425+cir52@users.noreply.github.com>
2023-06-01 12:44:16 -0300

    changed env variables for deploying

--- src/sanity/sanityContext.js
+++ src/sanity/sanityContext.js
@@ -1,10 +1,16 @@
-import { createClient } from "next-sanity";
+import { createClient } from 'next-sanity'
 
-const sanityClient = createClient({
-  projectId: 'uf77088s',
-  dataset: 'production',
-  apiVersion: '2023-04-24',
-  useCdn: true,
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-22'
+
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const useCdn = true;
+const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+
+export const sanityClient = createClient({
+  apiVersion,
+  dataset,
+  projectId,
+  useCdn,
+  token
 })
-
-export { sanityClient }
