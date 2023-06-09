82e02bbcaeaca525b9056cfbe68cadd7a40cb021
cir52 <47673425+cir52@users.noreply.github.com>
2023-06-09 14:24:45 -0300

    disabled sanity cache

--- src/sanity/sanityContext.js
+++ src/sanity/sanityContext.js
@@ -4,7 +4,7 @@ const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-22'
 
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const useCdn = true;
+const useCdn = false;
 const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
 
 export const sanityClient = createClient({
