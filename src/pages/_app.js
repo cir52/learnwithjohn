8d0fdd461397fe3dea472fb39e23a3f6796dcf76
cir52 <47673425+cir52@users.noreply.github.com>
2023-06-17 20:59:45 -0300

    bugtest2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 
 import '@/styles/globals.css'
 import '@/styles/styles.css'
-import { SessionProvider } from "next-auth/react"
+//import { SessionProvider } from "next-auth/react"
 import { Open_Sans } from 'next/font/google'
 
 const openS = Open_Sans({
@@ -9,15 +9,15 @@ const openS = Open_Sans({
   weight: ['300', '400', '500', '600', '700'],
 })
 
-export default function App({ session, Component, pageProps }) {
-
+// export default function App({ session, Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return ( 
-    <SessionProvider session={session}>
+    // <SessionProvider session={session}>
 
         <div className={openS.className} >
             <Component  {...pageProps} />
         </div>
 
-    </SessionProvider>
+    // </SessionProvider>
   )
 }
